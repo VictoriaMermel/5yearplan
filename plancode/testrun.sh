bc5334d7cc648828c6736463a91de72e53bbf468
Victoria Mermel <iris1917@protonmail.com>
2022-12-04 18:02:59 -0600

    Modified testrun.sh so word count of model.lp is labeled and degree of plan fulfilment is printed at the end.

--- plancode/testrun.sh
+++ plancode/testrun.sh
@@ -7,8 +7,14 @@ java planning.testeconomy $1 $2
 java planning.nyearplan testflow.csv testcap.csv testdep.csv testtarg.csv > /tmp/model.lp
  
 echo sectors $1 years $2
+echo lines, words, chars
 wc /tmp/model.lp  
- 
+
+echo execute and time  the linear programme
+echo 'time lp_solve < /tmp/model.lp | sort | sed "/Actual/d/" > plan.txt'
 time lp_solve < /tmp/model.lp | sort | sed "/Actual/d" > plan.txt
+
+echo Degree of plan fulfillment using linear programme
+tail --lines=2 plan.txt
 
 rm testflow.csv testcap.csv testdep.csv testtarg.csv
